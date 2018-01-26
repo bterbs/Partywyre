3e1cffd81f035f50f4bb7756938721bde75eb439
B Terbush <brittanyterbush@gmail.com>
2018-01-26 14:26:11 -0800

    Configure routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const routes = require('./server/routes')
 
 const app = express()
@@ -8,24 +7,14 @@ const app = express()
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 
-
+//Middleware:
 // to parse incoming requests
-// app.use(express.cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // to serve static files
 app.use(express.static('public'))
 
-// // to parse incoming requests
-// app.use(bodyParser.urlencoded({ extended: false }))
-
-
-app.use(methodOverride('_method'))
-
-app.use(middlewares.setDefaultResponseLocals)
-
 app.use('/', routes)
-// app.use('/contacts', contacts)
 
 app.use((request, response) => {
   response.render('common/not_found')
