7d795d2ced561cefadf07e3f0783860a79e13b02
B Terbush <brittanyterbush@gmail.com>
2018-01-31 14:07:59 -0800

    Update index.ejs to add message variable above login form

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,15 +2,19 @@ const router = require('express').Router();
 const doesPasswordExist = require('../model/db/events.js')
 
 router.get('/', (req, res, next) => {
-  return res.render('events/index.ejs')
+  return res.render('events/index.ejs', {message:'There\'s a party tonight!'})
 })
 
 router.post('/', (req, res, next) => {
   const password = req.body.password
   doesPasswordExist(password)
     .then((event) => {
-      console.log(`the party location is ${event.event_location}`)
-      return res.render('events/eventRabbit.ejs', {time: event.event_time, location: event.event_location, artists: event.event_artists})
+      if (!event) {
+        res.render('events/index.ejs', { message: 'Sorry, were you invited? Try again.'})
+      } else {
+        console.log(`the party location is ${event.event_location}`)
+        return res.render('events/eventRabbit.ejs', {time: event.event_time, location: event.event_location, artists: event.event_artists})
+      }
     })
     .catch(console.error)
 })
