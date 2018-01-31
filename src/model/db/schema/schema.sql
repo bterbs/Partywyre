337bd20a28825f2ba7151be25d9bfed2e0b8ca3b
B Terbush <brittanyterbush@gmail.com>
2018-01-30 16:44:33 -0800

    Add favicon. Create db schema

--- src/model/db/schema/schema.sql
+++ src/model/db/schema/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS admin_users;
+DROP TABLE IF EXISTS events;
 
 CREATE TABLE admin_users (
   admin_id SERIAL PRIMARY KEY,
@@ -6,4 +7,11 @@ CREATE TABLE admin_users (
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   role VARCHAR(255) DEFAULT 'party_thrower'
+);
+
+CREATE TABLE events (
+  event_id SERIAL PRIMARY KEY,
+  event_password VARCHAR(255),
+  event_location VARCHAR(255),
+  event_notes VARCHAR(255)
 );
---
+++ src/model/db/schema/seed.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO
+  events (event_password, event_location, event_notes)
+VALUES
+  ('rabbit', 'Oakland', 'From 2am til sunrise. BYOB. Perfomances by BrittanyFromEarlier and friends.')
